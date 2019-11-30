f430830760db0ac31f03bf5799f5d6235630a626
Carolyn Mei <mcarolyn@mit.edu>
2019-11-29 20:03:45 -0500

    Add yeeting endpoint

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home_page():
     return app.send_static_file('yeet.html')
 
+@app.route('/yeeting')
+def home_page():
+    return app.send_static_file('yeeting.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=int(os.environ.get('PORT', 8080)))
